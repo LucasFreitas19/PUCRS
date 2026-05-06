7d3d26812644dcd6df97c7ce128b2fe2f3f5b818
unknown <lucas.carmo2003@gmail.com>
2026-05-06 00:21:07 -0300

    AjustesFinosChekcout

--- LANDING/checkout.js
+++ LANDING/checkout.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 
                 // Redireciona para o painel principal (aplicação) após 1 segundo
                 setTimeout(() => {
-                    window.location.href = 'index.html';
+                    window.location.href = 'https://lucasfreitas19.github.io/PUCRS/APP/index.html';
                 }, 1000);
                 
             }, 2000);
